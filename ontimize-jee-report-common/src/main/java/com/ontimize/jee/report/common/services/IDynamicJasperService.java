dc7bdb0bdff473f0f588880ea5db42d6a3c6e17e
Tomas Fuentes <tomasfacal@gmail.com>
2022-06-07 11:43:46 +0200

    Modified exception

--- ontimize-jee-report-common/src/main/java/com/ontimize/jee/report/common/services/IDynamicJasperService.java
+++ ontimize-jee-report-common/src/main/java/com/ontimize/jee/report/common/services/IDynamicJasperService.java
@@ -15,7 +15,8 @@ public interface IDynamicJasperService {
 
 	public DynamicReport buildReport(List<String> columns, String title, List<String> groups, String entity,
 			String service, Boolean vertical, List<String> functions, List<String> style, String subtitle,
-			List<ColumnDto> columnsDto, String language, List<ServiceRendererDto> serviceRendererList) throws Exception;
+			List<ColumnDto> columnsDto, String language, List<ServiceRendererDto> serviceRendererList)
+			throws DynamicReportException;
 
 	public InputStream createReport(ReportParamsDto param) throws DynamicReportException;
 
